808227875cb59547a4555e6e8d89a43e5a7ffaed
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-07 00:12:52 +0000

    created my own too to determine equality of variables.

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log('❌❌❌ Assertion Failed: ' + actual + '!== ' + expected);
+  } else {
+    console.log('✅✅✅ Assertion Passed: ' + actual + '=== ' + expected);
+  }
+};
+
+
+assertEqual('lighthouse labs', 'bootcamp');
