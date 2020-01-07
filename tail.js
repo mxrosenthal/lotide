6b1d3acab43d9a9ca68699136ad45ce4276eb37e
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-07 01:44:27 +0000

    tail returns everything but the first index of an array.

---
+++ tail.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    // console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    // console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let end = [];
+  if (array.length <= 1) {
+
+    return end;
+  }
+
+  for (let i = 1; i < array.length; i++) {
+    end.push(array[i]);
+  }
+  return end;
+};
+
+
+// console.log(tail(['big','small','huge',8]));
+console.log(tail(['bub']));
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);// no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);// original array should still have 3 elements!
+assertEqual(tail([15]),[]);
+
+console.log(assertEqual([], []));
