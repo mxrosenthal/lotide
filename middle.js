79b63029fea477a409a027c861b3b6fbd4d9f3ba
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-08 02:21:37 +0000

    improved readability.

--- middle.js
+++ middle.js
@@ -28,16 +28,20 @@ const assertArraysEqual = (arr1, arr2)=> {
 const middle = (array)=> {
   let middleVal = [];
   let middleInd;
+  
   if (array.length >= 0 && array.length <= 2) {
     return middleVal;
+
   } else if (array.length % 2 === 0) {
     middleInd = (((array.length) / 2) - 1);
     middleVal.push(array[middleInd]);
     middleVal.push(array[middleInd + 1]);
+
   } else if (array.length % 2 !== 0) {
     middleInd = ((array.length - 1) / 2);
     middleVal.push(array[middleInd]);
   }
+
   return middleVal;
 };
 // console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
