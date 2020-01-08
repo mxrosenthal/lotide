c45239153956211a5291198074c9fd753387a8e2
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-08 22:07:30 +0000

    initial commit. Just copied over assertArraysEquals

---
+++ findKeyByValue.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = (arr1, arr2)=> {
+  console.log(arr1);
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
