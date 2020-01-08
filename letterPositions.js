6d254a9facab73d3a7b45e9fed4e64eb643a6ba4
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-08 21:44:45 +0000

    implementedletterPositions function but haven't got my tests working yet.

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  ind = 0;
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      
+      if (results[letter]) {
+        results[letter].push(ind);
+      } else {
+        results[letter] = [];
+        results[letter].push(ind);
+      }
+    }
+    ind++;
+  }
+
+  return results;
+};
+
+const eqArrays = (arr1, arr2)=> {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2)=> {
+  console.log(arr1);
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// console.log(assertArraysEqual((letterPositions('hello')), ))
+console.log(letterPositions('temporary living space.'));
