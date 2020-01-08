52d1d4c3c637946a95953bb7ada6a4561897e720
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-08 22:36:22 +0000

    implemented the function and all tests pass.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,3 +6,36 @@ const assertArraysEqual = (arr1, arr2)=> {
     console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+
+const findKeyByValue = function(obj, val) {
+  for (const item in obj) {
+    if (obj[item] === val) {
+      return item;
+    }
+  }
+  return;
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+
+// console.log(findKeyByValue(bestTVShowsByGenre));
+// console.log(findKeyByValue(bestTVShowsByGenre));
