9c25d288a7f7e0da045a44b6154a46cecf51fe0a
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-09 00:28:11 +0000

    lint rolled

--- eqObjects.js
+++ eqObjects.js
@@ -20,21 +20,7 @@ const eqArrays = (arr1, arr2)=> {
 
 const eqObjects = function(obj1, obj2) {
   let obj1keys = Object.keys(obj1);
-  let obj1vals = [];
   let obj2keys = Object.keys(obj2);
-  let obj2vals = [];
- 
-  for (const key of Object.keys(obj1)) {
-    obj1vals.push(obj1[key]);
-  }
-  for (const key of Object.keys(obj2)) {
-    obj2vals.push(obj2[key]);
-  }
-
-  // console.log('obj1 key length', obj1keys.length);
-  // console.log('obj1 val length', obj1vals.length);
-  // console.log('obj2 key length', obj2keys.length);
-  // console.log('obj2 val length', obj2vals.length);
 
   obj1keys = obj1keys.sort();
   obj2keys = obj2keys.sort();
@@ -43,8 +29,6 @@ const eqObjects = function(obj1, obj2) {
     return false;
   } else if (eqArrays(obj1keys, obj2keys)) {
     for (const key of obj1keys) {
-      console.log('object 1 value', obj1[key]);
-      console.log('object 2 value', obj2[key]);
 
       if (Array.isArray(obj1[key]) && eqArrays(obj1[key], obj2[key])) {
         return true;
@@ -53,7 +37,7 @@ const eqObjects = function(obj1, obj2) {
       if (obj1[key] !== obj2[key]) {
         return false;
       }
-    }     
+    }
     return true;
 
   } else {
@@ -63,12 +47,12 @@ const eqObjects = function(obj1, obj2) {
 };
 
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true); // => true
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false); // => false
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
