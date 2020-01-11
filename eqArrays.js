ceb7ed63971c33443cf66129131adf21df78de45
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-11 03:54:38 +0000

    refactored to use recursion.

--- eqArrays.js
+++ eqArrays.js
@@ -6,31 +6,38 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-// assertEqual('lighthouse labs', 'bootcamp');
-// assertEqual(1, 1);
-
-
 const eqArrays = (arr1, arr2)=> {
   if (arr1.length !== arr2.length) {
     return false;
   }
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    
+    if (Array.isArray(arr1[i]) && !eqArrays(arr1[i], arr2[i])) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } 
+    if (arr1[i] !== arr2[i] && !Array.isArray(arr1[i])) {
       return false;
     }
   }
   return true;
 };
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => tr  ue
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => tr  ue
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
 
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
