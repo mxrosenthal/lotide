f8f6d5a31f1ba41d05727e948d04ffa9c2d6ed3f
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-11 03:08:31 +0000

    eqObjects uses recursion now and no longer needs equalArray for it to function!

--- eqObjects.js
+++ eqObjects.js
@@ -6,56 +6,62 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = (arr1, arr2)=> {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+const eqObjects = function(obj1, obj2) {
+    //gets object keys and puts them into array!
+    let obj1keys = Object.keys(obj1);
+    let obj2keys = Object.keys(obj2);
+
+    obj1keys = obj1keys.sort();
+    obj2keys = obj2keys.sort();
+
+    //are the arrays of keys equal length? if no, return false
+    if (obj1keys.length !== obj2keys.length) {
       return false;
     }
-  }
-  return true;
-};
 
-const eqObjects = function(obj1, obj2) {
-  let obj1keys = Object.keys(obj1);
-  let obj2keys = Object.keys(obj2);
-
-  obj1keys = obj1keys.sort();
-  obj2keys = obj2keys.sort();
-
-  if (obj1keys.length !== obj2keys.length) {
-    return false;
-  } else if (eqArrays(obj1keys, obj2keys)) {
+    //loop through each key of obj1
     for (const key of obj1keys) {
 
-      if (Array.isArray(obj1[key]) && eqArrays(obj1[key], obj2[key])) {
-        return true;
-      }
+      //is the value of obj1 and object? if yes, recursively call eqObjects to check 
+      //whether the nested obj's are unequal. if both are tru, return false
+      if (typeof obj1[key] === 'object' && !eqObjects(obj1[key], obj2[key])) {
+        
+        //if the values at the same key in obj1 not equal to obj2 return false
+        if (obj1[key] !== obj2[key]) {
+          return false;
+        } 
 
-      if (obj1[key] !== obj2[key]) {
-        return false;
-      }
-    }
+        return false;  
+      }  
+    }  
+
+    //if all previous testing didn't return false, then the 
+    //objects must be identical.
     return true;
-
-  } else {
-    return false;
-  } 
 };
 
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// // assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
+
+// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) ;// => true
+
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+console.log(eqObjects({ben: {ben: {ben: 'david'}}}, {ben: {ben: {ben: 'david'}}}))
