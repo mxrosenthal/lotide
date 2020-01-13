dac588252727d643db702c85a45bd66adf087102
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-13 22:44:54 +0000

    shortened index.js using shorthand syntax from ES6

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const flatten = require('./flatten');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+
+
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  without: this.without,
-  takeUntil: takeUntil,
-  map: map,
-  letterPosition: letterPosition,
-  flatten: flatten,
-  findKeyByValue: findKeyByValue,
-  findKey: findKey,
-  eqObjects: eqObjects,
-  eqArrays: eqArrays,
-  countOnly: countOnly,
-  countLetters: countLetters,
-  assertObjectsEqual: assertObjectsEqual,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual
+  head,
+  tail,
+  middle,
+  without,
+  takeUntil,
+  map,
+  letterPositions,
+  flatten,
+  findKeyByValue,
+  findKey,
+  eqObjects,
+  eqArrays,  
+  countOnly,
+  countLetters,
 }
