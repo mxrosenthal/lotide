838efb5745676d8fad038abadabb5bc23cfc40a8
Max Rosenthal <mxrosenthal@yahoo.com>
2020-01-13 20:38:09 +0000

    modularized middle.js. now exports middle.js to the module. test files save in test/middleTest.js

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-const eqArrays = (arr1, arr2)=> {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2)=> {
-  console.log(arr1);
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-// console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', '3']));
-// console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', 3]));
-
-// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-// console.log(assertArraysEqual([1, 2, 3], [1, 2, 4]));
-
 const middle = (array)=> {
   let middleVal = [];
   let middleInd;
@@ -44,11 +17,8 @@ const middle = (array)=> {
 
   return middleVal;
 };
-// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(middle([1, 2, 3]));
-// console.log(middle([1, 2]));
-// console.log(middle([1]));
 
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5,6]));
+module.exports = middle;
+
+
+
